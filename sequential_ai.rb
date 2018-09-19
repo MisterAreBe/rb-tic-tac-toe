abd5b1dfffaa8298105be2467371072c8701eaa8
Ricky Collins <richardcollins52317@gmail.com>
2018-09-19 14:46:52 -0400

    test passed

--- sequential_ai.rb
+++ sequential_ai.rb
@@ -2,6 +2,16 @@ require_relative 'base_ai.rb'
 
 class Sequential_ai < Base_ai
 
-    
+    def place_sequential()
+        x = 0; y = 0
+        while place_piece(x, y) == false
+            if y < 2 && y >= 0
+                y += 1
+            elsif y == 2
+                y = 0
+                x += 1
+            end
+        end
+    end
 
 end
