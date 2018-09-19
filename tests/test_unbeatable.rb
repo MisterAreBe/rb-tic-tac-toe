c5c36c52e55ef8706d7f14fc15676d75dfbc0235
Ricky Collins <richardcollins52317@gmail.com>
2018-09-19 14:51:08 -0400

    test passed, moving on to unbeatable ai

---
+++ tests/test_unbeatable.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "../unbeatable_ai.rb"
+
+class UnbeatableAi < Minitest::Test
+
+    def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
+
+end
