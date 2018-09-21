f64e0561a7204d22bc0387784eb56cb1f597a878
Ricky Collins <richardcollins52317@gmail.com>
2018-09-21 10:56:51 -0400

    test passed

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -40,6 +40,26 @@ class Unbeatable_ai < Base_ai
             collum += 1
         end
 
+        row = 0
+        collum = 0
+        counter = 0
+        temp = Hash.new
+        temp['taken'] = []; temp['grab'] = []
+        while counter < @board.size
+            if @board.grid[row][collum] == @piece
+                temp['taken'] << [row, collum]
+            else
+                temp['grab'] << [row, collum]
+            end
+            row += 1
+            collum += 1
+            counter += 1
+        end
+        if temp['taken'].length == 2 && temp['grab'].length == 1
+            x = temp['grab'][0][0]; y = temp['grab'][0][1]
+            return place_piece(x, y)
+        end
+
     end
 
 end
