8d992dc74afe7ab16b4ec7659bfc9ebde28a06b8
Ricky Collins <richardcollins52317@gmail.com>
2018-10-24 09:48:17 -0400

    test passed

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -73,7 +73,7 @@ class Unbeatable_ai < Base_ai
             end
             row += 1; collum -= 1; counter += 1
         end
-        if temp['taken'].length == 2 && temp['grab'].length == 1
+        if temp['taken'].length == (@board.size - 1) && temp['grab'].length == 1
             x = temp['grab'][0][0]; y = temp['grab'][0][1]
             if @board.check_place(x, y)
                 return place_piece(x, y)
